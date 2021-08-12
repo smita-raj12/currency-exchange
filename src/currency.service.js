86b4cc715dee6e3888fdcefc0f273ab6f58221c0
smita Raj <smita.raj12@gmail.com>
2021-08-12 13:18:38 -0700

    create an application for currency exchange

---
+++ src/currency.service.js
@@ -0,0 +1,14 @@
+export class CurrencyService {  
+  static getCurrency() {
+    return fetch  (`https://v6.exchangerate-api.com/v6/${process.env.API_}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
