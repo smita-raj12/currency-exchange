73fe27f14b472576a428d9c24c7bcab7e07cdfc6
smita Raj <smita.raj12@gmail.com>
2021-08-12 16:49:36 -0700

    add functionality to convert ud doller to user selected country

--- src/index.js
+++ src/index.js
@@ -7,22 +7,44 @@ import CurrencyService from './currency.service.js';
 function clearFields() {
   $('#currency').text("");
   $('.showErrors').text("");
-  $('.rupees').text("");
+  $('.amount').text("");
 }
 
 function getElements(response) {
-    let amount =  parseInt($('#currency').val());
-    if(response.conversion_rates.USD >= 1){
-      const indianRupees = amount * response.conversion_rates.INR;
-      $('#rupees').text(indianRupees);
-    } else {
-      $('.showErrors').text(`There was an error: ${response.message}`);
-    }
+  let amount =  parseInt($('#currency').val());
+  let selectedCountry = $("#countries").val();
+  if(response.conversion_rates.USD >= 1 && selectedCountry === "INR"){
+    const convertedIndianAmount = amount * response.conversion_rates.INR;
+    $('#amount').text(convertedIndianAmount);
+  }
+
+  if(response.conversion_rates.USD >= 1 && selectedCountry === "LYD"){
+    const convertedLibiyaAmount = amount * response.conversion_rates.LYD;
+    $('#amount').text(convertedLibiyaAmount);
+  } 
+
+  if(response.conversion_rates.USD >= 1 && selectedCountry === "SSP"){
+    const convertedSudanAmount = amount * response.conversion_rates.SSP;
+    $('#amount').text(convertedSudanAmount);
+  } 
+
+  if(response.conversion_rates.USD >= 1 && selectedCountry === "SYP"){
+    const convertedSyriaAmount = amount * response.conversion_rates.SYP;
+    $('#amount').text(convertedSyriaAmount);
+  }
+
+  if(response.conversion_rates.USD >= 1 && selectedCountry === "MUR"){
+    const convertedMauritiusAmount = amount * response.conversion_rates.MUR;
+    $('#amount').text(convertedMauritiusAmount);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  } 
 }
 
 $(document).ready(function() {
   $('#currencycheck').click(function() {
     let amount = $('#currency').val();
+    
     clearFields();  
     CurrencyService.getCurrency(amount)
       .then(function(response) {
