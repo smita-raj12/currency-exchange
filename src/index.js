e6a79ae81a17ca3275da225cd237cd089e9833b3
smita Raj <smita.raj12@gmail.com>
2021-08-14 11:24:33 -0700

    display error output

--- src/index.js
+++ src/index.js
@@ -10,10 +10,11 @@ function getElements(response){
     $("#code").text(response.target_code);
     $("#rate").text(response.conversion_rate);
     $("#amount").text(response.conversion_result);
-    
+    $("#update").text(response.time_last_update_utc);
+    $("#next").text(response.time_next_update_utc)
   }else{
-    $("#output").hide();
     $(".showErrors").html('There was an error in your selected input,' +'<br>' + 'Please select valid amount and country name');
+    $("#output").hide();
   }  
 }
 
@@ -24,6 +25,7 @@ $(document).ready(function() {
     let selectedCountry = $("#countries").val();
     CurrencyService.getCurrency(selectedCountry,amount)
       .then(function(response) {
+        $("#output").show();
         getElements(response);
       });
   });
